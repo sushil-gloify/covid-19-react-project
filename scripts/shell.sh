4dea58ddf329c29246b1eb9152fd55e06de897af
sushil-gloify <102666277+sushil-gloify@users.noreply.github.com>
2023-02-07 19:40:50 +0530

    Update shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 # Set ownership for all folders and files
 
-chown -R ec2-user:ec2-user /home/ec2-user/server/scripts/after_install.sh
-chrp -R ec2-user:ec2-user /home/ec2-user/server/scripts/after_install.sh
 chmod 777 /home/ec2-user/server/scripts/after_install.sh
 
-chown -R ec2-user:ec2-user /home/ec2-user/server/scripts/app_start.sh
-chrp -R ec2-user:ec2-user /home/ec2-user/server/scripts/app_start.sh
 chmod 777 /home/ec2-user/server/scripts/app_start.sh
 
-chown -R ec2-user:ec2-user /home/ec2-user/server/scripts/before_install.sh
-chrp -R ec2-user:ec2-user /home/ec2-user/server/scripts/before_install.sh
 chmod 777 /home/ec2-user/server/scripts/before_install.sh
